52ae7539d22c4de4c2a65c3801bca5a5dfcbca7b
Maxwell Easter <maxwelleaster00@gmail.com>
2025-07-16 21:05:43 +0100

    update desc.js

--- desc.js
+++ desc.js
@@ -6,23 +6,29 @@ version 1:
     -create planet (bubble) & set state + metadata 
     -planet content 
     -implement stars 
+    -dynamic orbit lines 
     -customize planet (drag/drop, buttons)
     -implement visit request + permit 
     -welcome/intro script
+    -planet scribe (
+        planet age, name, id, links, created at, motto
+        star & visit count, galaxy,  )
     -algorithm (similar planets drift closer)
-    -spatial nav system (camera fly, zoom)
+    -spatial nav system (camera orbit, zoom)
+    -realtime log (menu[list] of planets & galaxy currently close to you)
+    -real-time clock (displays time in users planet)
     -notifications 
     -settings 
 
 
 version 2: 
-    -advanced planet customizer (via code, mods)
+    -advanced planet customizer (mods & imports)
     -celeb-owned planets + tour fee 
 
     
 version 3: 
-    -dynamic lighting based on time of day 
-    -real-time clock (displays time in users planet)
+    -planet weather 
+    -dream mode (night mode [dream like interactions])
 
 
 version 4: 
@@ -47,6 +53,20 @@ version 8:
     -build mode (add/import 3d spots to your bubble)
 
 
+version 9: 
+    -ai npcs 
+
+
+version 10: 
+    -miniverse studio 
+    -customize planet via code
+
+
+version 11: 
+    -adverse + analytics + shopping logs (
+        galaxies where companies can sell & advertise in their own planet
+    )
+
 --
 
 
@@ -66,6 +86,7 @@ The MiniVerse:
 Galaxies: 
     -communities (users can explore other galaxies based on preference)
     -event center (anybody can visit [game jams, concerts, sports etc])
+    -adverse (find your fav ocmpany and shop from there)
 
 
 Planets: 
@@ -77,13 +98,15 @@ Planets:
         [similar concepts to followers]
     )
     -state (idle [no connect], active [open to connect])
-    -metadata (dp name, id, state, stars count)
+    -metadata (dp name, id, state, stars count [on hover])
+    -scribe (logs a scroll that displays details about the planet)
     -content (
         social media links, user avatar, music embeds (spotify), readme (about)
         portfolio + blog, art page, img/vid, chat, NFTs 
     )
     -size (determined by number of connect)
     -tour fee - paid by users who visit celeb-owned planets 
+    -invites - other users [friends you know] can invite you to their planets 
 
 
 
@@ -93,22 +116,25 @@ FAQ:
         img/vid gallery, access their website (portfolio + blog) or art page 
         without leaving their world, chat terminal, live stream station 
 
+    -spatial audio - when you enter a planet, you can hear the song the planet's 
+        owner is listening to in real-time (they can choose to turn it off)
+
     -3d spots (users can charge for specific spots)
-    -3d character
+    -3d characters
 
 
 
 Sample Tech Stack:  
     frontend {
         threejs, webgl 
-        react, electron
+        react, zustand 
         tailwind 
     }
 
     backend {
         postgre, prisma 
         node, epxress
-        socket, webrtc 
+        socket, livekit 
         redis 
         s3, cloudinary
     }
@@ -116,7 +142,6 @@ Sample Tech Stack:
     ai & sync {
         web audio api 
         openai
-        convex 
     }
 
     web3 layer {
@@ -141,4 +166,33 @@ Sample Tech Stack:
         mobile/desktop pwa 
     }
 
+    dev team {
+        frontend [3d, miniverse, components]
+        backend [database, api, architecture]
+        real-time [chat, streams, presence]
+        ai [chatbots, openai, custom agents]
+        web3 [nft, smart contracts, crypto]
+    }
+
+
+
+Monetization Strategies:
+    Take x% of
+    -asset trades 
+    -nft trades 
+    -ai agent purchases 
+    -tour fees 
+    -adverse revenue 
+    -event center revenue 
+
+    Miniverse studio sub
+    -advanced editor tools 
+    -scripting libs 
+    -ai-assist code gen
+    -premium planet mods & themes
+    -premium spots 
+
 */
+
+
+
